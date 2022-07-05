d69d6e17298b7a724d922ee691766155932e5e78
Sika DALI <sika.dali@telecomnancy.net>
2022-07-05 10:38:57 +0200

    first commit for backend

---
+++ src/main/java/com/sikadali/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.sikadali.springboot.repository;
+
+import com.sikadali.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
